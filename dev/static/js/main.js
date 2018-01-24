34f489e9c7d9f9cc923d3f36fb09f0227c8d071c
3RMc <ermakoff1994@mail.ru>
2018-01-25 01:12:48 +0300

    Свёрстано и адаптировано боковое меню

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,2 +1,5 @@
-;
-// Начинать писать отсюда!!!!
+function change(element) {
+    element.classList.add( "menu-active");
+    var layer = document.getElementById("menu-layer");
+    layer.classList.add( "menu-layer_active");
+}
